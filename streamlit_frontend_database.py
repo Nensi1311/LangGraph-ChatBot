031a30b8eb09a3164aa5c99adc85e6829b9b2b75
Nensi Pansuriya <nensipansuriya1311@gmail.com>
2025-11-11 11:19:29 +0530

    CONFIG variable updated to integrate with langsmith

--- streamlit_frontend_database.py
+++ streamlit_frontend_database.py
@@ -72,7 +72,12 @@ if user_input:
     with st.chat_message("user"):
         st.text(user_input)
 
-    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+    # CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+    CONFIG = {
+        "configurable": {"thread_id": st.session_state["thread_id"]},
+        "metadata": {"thread_id": st.session_state["thread_id"]},
+        "run_name": "Chat_Turn",
+    }
 
     with st.chat_message("assistant"):
         ai_message = st.write_stream(
